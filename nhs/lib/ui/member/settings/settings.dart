c8228992cb1530e99850c12f858686acebe7f6c3
Matthew Greenspun <78003195+MatthewGreenspun@users.noreply.github.com>
2023-04-02 21:02:57 -0400

    Add settings page

--- nhs/lib/ui/member/settings/settings.dart
+++ nhs/lib/ui/member/settings/settings.dart
@@ -1,12 +1,70 @@
 import 'package:flutter/material.dart';
+import "package:flutter_mobx/flutter_mobx.dart";
+import "../../../stores/member.store.dart";
+import "package:provider/provider.dart";
+import '../../shared/settings/settings_container.dart';
+import '../../shared/settings/setting.dart';
+import "../../../utils/auth.dart";
+import "../../../utils/fmt.dart";
 
 class Settings extends StatelessWidget {
   const Settings({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Settings"),
-    );
+    return Consumer<MemberStore>(
+        builder: (_, memberStore, __) => Observer(
+            builder: (context) => Padding(
+                padding: const EdgeInsets.all(8),
+                child: ListView(children: [
+                  SettingsContainer(
+                    title: "Account",
+                    child: Column(children: [
+                      Setting(
+                          name: "Display Name",
+                          child: Text(memberStore.member!.name)),
+                      Setting(
+                          name: "Email",
+                          child: Text(memberStore.member!.email)),
+                      Setting(
+                          name: "Graduation Year",
+                          child: Text(
+                              memberStore.member!.graduationYear.toString())),
+                      Setting(
+                          name: "Sign Out",
+                          child: OutlinedButton(
+                            style: ButtonStyle(
+                                overlayColor: MaterialStateProperty.all(
+                                    const Color.fromARGB(82, 244, 67, 54)),
+                                foregroundColor:
+                                    MaterialStateProperty.all(Colors.red)),
+                            child: const Text("Sign Out"),
+                            onPressed: () {
+                              Authentication.signOut(context: context).then(
+                                  (value) => Navigator.pushReplacementNamed(
+                                      context, "sign-in"));
+                            },
+                          ))
+                    ]),
+                  ),
+                  SettingsContainer(
+                      title: "Tutoring Subjects",
+                      isEditable: true,
+                      child: Column(
+                        children: memberStore.member!.tutoringSubjects
+                            .map((subject) =>
+                                Setting(name: subject, child: Container()))
+                            .toList(),
+                      )),
+                  SettingsContainer(
+                      title: "Free Periods",
+                      isEditable: true,
+                      child: Column(
+                          children: memberStore.member!.freePeriods.entries
+                              .map((entry) => Setting(
+                                  name: "Period ${entry.key}",
+                                  child: Text(fmtDays(entry.value))))
+                              .toList()))
+                ]))));
   }
 }
---
+++ nhs/lib/ui/shared/settings/settings_container.dart
@@ -0,0 +1,39 @@
+import "package:flutter/material.dart";
+
+class SettingsContainer extends StatelessWidget {
+  final String title;
+  final Widget child;
+  final bool isEditable;
+  const SettingsContainer(
+      {super.key,
+      this.title = "",
+      required this.child,
+      this.isEditable = false});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: const EdgeInsets.symmetric(vertical: 24),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(title,
+                  style: const TextStyle(
+                      fontSize: 20, fontWeight: FontWeight.bold)),
+              isEditable
+                  ? IconButton(onPressed: () {}, icon: const Icon(Icons.edit))
+                  : Container()
+            ],
+          ),
+          Container(
+              margin: const EdgeInsets.symmetric(vertical: 8),
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                  borderRadius: const BorderRadius.all(Radius.circular(8)),
+                  border:
+                      Border.all(color: Theme.of(context).hintColor, width: 3)),
+              child: child)
+        ]));
+  }
+}
